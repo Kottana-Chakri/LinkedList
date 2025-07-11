50b39e87bec7270dff4799db06a2a02f1a6c80fd
Kottana-Chakri <kottanachakri06@gmail.com>
2025-07-11 18:30:26 +0530

    introduction to linked list

---
+++ intro.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    public:
+    int val;
+    Node * next;
+
+    public:
+    Node(int val0,Node* val1){
+        val=val0;
+        next=val1;
+    }
+};
+class Solution{
+
+};
+int main(){
+    Node x = Node(2,nullptr);
+    cout << x.val << endl;
+    return 0;
+}
